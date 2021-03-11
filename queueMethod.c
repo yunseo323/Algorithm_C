6fa55c8a4d6c5e2528157e7dfbec1dab4cdaa87c
yunseo323 <jokani3244833@gmail.com>
2021-03-11 22:51:46 +0900

    큐

--- queueMethod.c
+++ queueMethod.c
@@ -1,34 +1,81 @@
-/*배열(크기 N의 배열을 원형으로 사용)*/
+/*연결리스트*/
+typedef struct CircularQueueNodeType
+{
+	char data;
+} CircularQueueNode;
 
 typedef struct _cirqueue {
-	int *arr; //큐배열
+	CircularQueueNode *pData; //큐배열
 	int front, rear;
-	int size;
+	int currentCount;
+	int maxCount;
 }CirQueue;
 
-void initQueue(CirQueue *arr, int N) { //큐 초기화
-	arr->size = N;
-	arr->front = 0;
-	arr->rear = arr->size - 1;
+void createCircularQueue(CirQueue *arr, int N) { //큐 초기화
+	CircularQueue *new = NULL;
+	new = (CirQueue *)malloc(sizeof(CirQueue));
+
+	new->maxCount = size;
+	new->front = -1;
+	new->rear = -1;
+
+	new->pData = (CirQueue *)malloc(sizeof(CirQueue));
+	return new;
 }
 
-int isEmpty(CirQueue *arr) {
+int isEmpty(CirQueue *Queue) {
+	int re = 0;
 
-}
-
-int isFull(CirQueue *arr) {
-
-}
-
-void enqueue(CirQueue *arr, int elem) {
-	if (isFull(arr)) {
-
+	if (Queue != NULL)
+	{
+		if (Queue->currentCount == 0)
+		{
+			re = 1;
+		}
 	}
+
+	return re;
 }
 
-int dequeue(CirQueue *arr) {
-	if (isEmpty(arr)) {
-
+int isFull(CirQueue *Queue) {
+	int re = 0;
+	if (Queue != NULL)
+	{
+		if (Queue->currentCount == Queue->maxCount)
+		{
+			re = 1;
+		}
 	}
+
+	return re;
 }
-/*연결리스트*/
+
+void enqueue(CirQueue *Queue, char data) {
+	if (Queue != NULL) {
+		if (isFull(Queue) == 0) {
+			pQueue->rear = (pQueue->rear + 1) % pQueue->maxCount;
+			pQueue->pData[pQueue->rear].data = data;
+			pQueue->currentCount++;
+		}
+		else {
+			printf("ERROR:enqueue");
+		}
+	}
+	
+}
+
+CircularQueueNode dequeue(CirQueue *Queue) {
+	CircularQueueNode *re = NULL;
+	if (Queue != NULL) {
+		if (isEmpty(Queue)) {
+			pQueue->front = (pQueue->front + 1) % pQueue->maxCount;
+			pReturn->data = pQueue->pData[pQueue->front].data;
+			pQueue->currentCount--;
+		}
+		else {
+			printf("ERROR:dequeue");
+		}
+	}
+
+	
+}
